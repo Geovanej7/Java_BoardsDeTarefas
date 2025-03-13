d7b910e6f1891e98896525a2bc80664a883ab98a
Geo <gjsj@discente.ifpe.edu.br>
2025-03-13 15:43:56 -0300

    Renaming branch main

--- src/main/java/com/br/board/controller/ColumnsController.java
+++ src/main/java/com/br/board/controller/ColumnsController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.br.board.controller.DTOs.ColumnsRequest;
 import com.br.board.model.columns.Columns;
 import com.br.board.model.columns.ColumnsService;
